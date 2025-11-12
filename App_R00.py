7b077c9b5c14ec3df6ca37c52e3e8d34861c9c26
ngoswami0607 <navjeetgoswami@gmail.com>
2025-11-12 10:10:44 -0600

    Update App_R00.py

--- App_R00.py
+++ App_R00.py
@@ -2,13 +2,17 @@ import sys
 import os
 import streamlit as st
 import traceback
+
+from auth import authenticate_user
 from functions.building_dimension import building_dimension
 from functions.code_jurisdiction_1 import code_jurisdiction_1
 from functions.risk_category import risk_category
 from functions.wind_speed import wind_speed
 from functions.wind_pressure_calc import wind_pressure_calc
 
-print(os.listdir("functions"))
+authenticate_user()
+
+# print(os.listdir("functions"))
 
 st.set_page_config(page_title="Wind Load Calculator", layout="centered")
 st.title("🌪️ Wind Load Calculator (ASCE 7)")
