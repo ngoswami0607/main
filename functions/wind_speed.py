12c33694f93ed983387570b778511ee88c4d59f8
ngoswami0607 <navjeetgoswami@gmail.com>
2025-12-12 21:10:05 -0600

    Update wind speed header from '4️⃣' to '5️⃣'

--- functions/wind_speed.py
+++ functions/wind_speed.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def wind_speed():
-    st.header("4️⃣ Wind Speed")
+    st.header("5️⃣ Wind Speed")
     st.markdown("Get your wind speed (V) from [ASCE Hazard Tool](https://ascehazardtool.org/)")
     V = st.number_input("Enter Basic Wind Speed (mph)", min_value=0.0, value=115.0)
     st.success(f"Using V = {V:.1f} mph")
