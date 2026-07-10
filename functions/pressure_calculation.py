447a85d399a14bd0150be6add7a604a55f539057
ngoswami0607 <navjeetgoswami@gmail.com>
2026-07-10 09:09:11 -0500

    Modify pressure calculation to scale by 0.6
    Updated pressure calculation to include a scaling factor of 0.6.

--- functions/pressure_calculation.py
+++ functions/pressure_calculation.py
@@ -1,7 +1,7 @@
 def calculate_pressure(q, gcp, gcpi_positive, gcpi_negative, sign):
 
     if sign=="positive":
-        return q*(gcp-gcpi_negative)
+        return 0.6*q*(gcp-gcpi_negative)
 
     elif sign=="negative":
-        return q*(gcp-gcpi_positive)
+        return 0.6*q*(gcp-gcpi_positive)
